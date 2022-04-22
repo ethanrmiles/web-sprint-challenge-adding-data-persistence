0c5810db709bc1d6bf23f71e76156aff94ddf391
Ethan Miles <ethanmilescs@gmail.com>
2022-04-22 13:48:33 -0500

    finished db schema

--- data/migrations/20220422171736_createtable.js
+++ data/migrations/20220422171736_createtable.js
@@ -3,7 +3,30 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('projects', t => {
+        t.increments('project_id')
+        t.string('project_name').notNullable()
+        t.string('project_description')
+        t.integer('project_completed')
+    })
+  .createTable('resources', t => {
+    t.increments('resource_id')
+    t.string('resource_name').unique().notNullable()
+    t.string('resource_description')
+  })
+  .createTable('tasks', t => {
+      t.increments('task_id')
+      t.string('task_description').notNullable()
+      t.string('task_notes')
+      t.integer('task_completed')
+      t.integer('project_id').unsigned().notNullable().references('project_id').inTable('projects')
+  })
+  .createTable('resource-assignment', t => {
+      t.integer('project_id').unsigned().notNullable().references('project_id').inTable('projects')
+      t.integer('resource_id').unsigned().notNullable().references('resource_id').inTable('resources')
+      t.primary(['project_id', 'resource_id'])
+  })
 };
 
 /**
